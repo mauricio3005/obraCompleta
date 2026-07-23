fa1705ef27cab9faaa294a42d2e82607d6d92893
mauricio3005 <mauriciomenezeslemos@gmail.com>
2026-07-23 19:37:42 -0300

    Criação do model pessoa

---
+++ src/main/java/com/mauricio/obracompleta/model/Pessoa.java
@@ -0,0 +1,43 @@
+package com.mauricio.obracompleta.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.validator.constraints.br.CPF;
+
+@Entity
+@Table(name = "pessoa")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Pessoa {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank
+    @Column(nullable = false)
+    private String nome;
+
+    @CPF
+    @Column(unique = true)
+    private String cpf;
+
+    private String telefone;
+
+    @Email
+    private String email;
+}
